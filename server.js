e1499ca2aa061bc77e3998af687e214fc164e5c6
Jake Schaefbauer <jschaefmn@gmail.com>
2022-07-27 08:26:48 -0500

    finished lesson 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,7 +21,7 @@ const sess = {
   })
 };
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
